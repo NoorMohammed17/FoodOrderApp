7ae8527296d8e8f5182442763613b20dc20abb3a
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-06 22:37:23 +0530

    hedaercartbutton added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import styles from "./Header.module.css";
 import mealsImage from "../../components/assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>Cart Meals</h1>
-        <button>Cart</button>
+       <HeaderCartButton/>
       </header>
       <div className={styles['main-image']}>
         <img alt="A table full of Meals" src={mealsImage} />
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import styles from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = () => {
+  return (
+    <button className={styles.button}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span className={styles.button}> Your Cart </span>
+      <span className={styles.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
