2930831badb95944dfaf646e36f905b8479d92c8
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-06 23:55:17 +0530

    new files added mealItem

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import classes from "./MealItemForm.module.css";
+import Input from '../../UI/Input';
+
+
+
+const MealItemForm = (props) => {
+
+
+
+  return (
+    <form className={classes.form} 
+    // onSubmit={submitHandler}
+    >
+      <Input
+        // ref={amountInputRef}
+        label='Amount'
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
+      {/* {!amountIsValid && <p>Please enter a valid amount (1-5).</p>} */}
+    </form>
+  );
+};
+
+export default MealItemForm;
